3e38e0904e1651003431cc422abc80048bf3f74b
Viet Duc Kieu <viet.duc.kieu@web.de>
2023-01-23 20:57:02 +0100

    deploy are three deep learning models

--- application/streamlit_app.py
+++ application/streamlit_app.py
@@ -1,19 +1,35 @@
 import streamlit as st
-import random
 import cv2
 import numpy as np
 import pickle
 
+# set page config (must be called as the first Streamlit command)
 st.set_page_config(
     page_title="Integrationsseminar",
     page_icon="👕",
 )
 
-# loading the saved model
-# uncomment the following line if you are running the app locally
-loaded_model = pickle.load(open("trained_model.sav", "rb"))
-# loaded_model = pickle.load(open("application/trained_model.sav", "rb"))
 
+# import model for deployment
+# load model with cache
+@st.cache(allow_output_mutation=True)
+def load_model_path():
+    model_linear = pickle.load(open("application/trained_model.sav", "rb"))
+    model_1_layer = pickle.load(open("application/model_1_layer.sav", "rb"))
+    model_2_layer = pickle.load(open("application/model_2_layer.sav", "rb"))
+    model_3_layer = pickle.load(open("application/model_3_layer.sav", "rb"))
+    return model_linear, model_1_layer, model_2_layer, model_3_layer
+
+model_linear, model_1_layer, model_2_layer, model_3_layer = load_model_path()
+
+
+# import model on local machine (UNCOMMENT BELOW WHEN RUNNING LOCAL)
+# model_linear = pickle.load(open("trained_model.sav", "rb"))
+# model_1_layer = pickle.load(open("model_1_layer.sav", "rb"))
+# model_2_layer = pickle.load(open("model_2_layer.sav", "rb"))
+# model_3_layer = pickle.load(open("model_3_layer.sav", "rb"))
+
+# dictionary for categories
 clothes_dict = {
     0: "T-shirt/top",
     1: "Trouser",
@@ -28,19 +44,42 @@ clothes_dict = {
 }
 
 
-def model_prediction(image):
-    category = loaded_model.predict(image)
-    category_name = clothes_dict[category[0]]
-    return category_name
+# linear model prediction
+def predict_linear(image):
+    # Predict probabilities for each category -> [0.01 0.01 0.2 0. 0.02 0. 0.17 0. 0.59 0. ]
+    probs = model_linear.predict_proba(image)[0]
+    # Get the index of the highest probability -> 8
+    max_prob_index = np.argmax(probs)
+    # Get the highest probability -> 0.59
+    max_prob = probs[max_prob_index]
+    # Get the category name -> Bag
+    category = clothes_dict[max_prob_index]
+    return category, max_prob
 
 
-def random_class(image):
-    # generate random number between 0 and 9
-    random_number = random.randint(0, 9)
+# Create a list to store the model
+cnn_models = [model_1_layer, model_2_layer, model_3_layer]
 
-    category = clothes_dict[random_number]
-    confidence = round(random.random(), 2)
-    return category, confidence
+
+def predict_nn(image):
+    # Reshape image to 1x28x28x1
+    image_reshape_nn = image.reshape(1, 28, 28, 1)
+
+    # Create a dictionary to store the predictions of each model
+    predictions = {}
+    # Iterate over the models
+    for i, model in enumerate(cnn_models):
+        # Predict the category with the current model
+        model_prediction = model.predict(image_reshape_nn)
+        # get the index where the highest value is
+        category = np.argmax(model_prediction)
+        # get the highest value
+        prob = np.max(model_prediction)
+        # Add the prediction to the dictionary
+        predictions["model_{}_category".format(i + 1)] = clothes_dict[category]
+        predictions["model_{}_prob".format(i + 1)] = prob
+
+    return predictions
 
 
 st.title("Fashion MNIST")
@@ -63,27 +102,36 @@ if img_file_buffer is not None:
 
     # resize image to 28x28
     st.write("Second step: resize image to 28x28")
-    cv2_img_2 = cv2.resize(cv2_img, (28, 28))
-    st.image(cv2_img_2, width=400)
-    st.write("Resized dimension: ", cv2_img_2.shape)
+    cv2_img_resized = cv2.resize(cv2_img, (28, 28))
+    st.image(cv2_img_resized, width=400)
+    st.write("Resized dimension: ", cv2_img_resized.shape)
 
     # image as numpy array
     st.write("The image as numpy array")
     st.write("Original shape: ", cv2_img.shape)
-    st.write("Image shape: ", cv2_img_2.shape)
-    st.write(cv2_img_2)
+    st.write("Resized shape: ", cv2_img_resized.shape)
+    st.write(cv2_img_resized)
 
     # flatten image
-    img_flatten = cv2_img_2.reshape(1, -1)
+    img_flatten = cv2_img_resized.reshape(1, -1)
     st.write("Flatten image: ", img_flatten.shape)
     st.write(img_flatten)
     st.write("Rescale values by dividing by 255: ")
-    img_flatten_2 = img_flatten / 255
-    st.write(img_flatten_2)
+    img_flatten_rescaled = img_flatten / 255
+    st.write(img_flatten_rescaled)
 
-    # category, confidence = random_class(img_flatten_2)
-    # st.write(f"Category: {category}")
-    # st.write(f"Confidence: {confidence}")
+    # predict category linear
+    category_linear, category_linear_prob = predict_linear(
+        img_flatten_rescaled)
 
-    category2 = model_prediction(img_flatten_2)
-    st.write(f"Category: {category2}")
+    # predict category neural network
+    predictions_nn = predict_nn(cv2_img_resized)
+
+    st.write(
+        f"Category linear: {category_linear} with probability: {category_linear_prob}")
+    st.write(
+        f"Category 1 CNN layer: {predictions_nn['model_1_category']} with probability: {predictions_nn['model_1_prob']}")
+    st.write(
+        f"Category 2 CNN layer: {predictions_nn['model_2_category']} with probability: {predictions_nn['model_2_prob']}")
+    st.write(
+        f"Category 3 CNN layer: {predictions_nn['model_3_category']} with probability: {predictions_nn['model_3_prob']}")
